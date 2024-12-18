06c26ab597f156c8f5c2f507070727068bfe9273
angelmanuel0302 <angelmanuel0302@gmail.com>
2024-12-18 10:42:16 +0800

    Updated corsOption

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-	origin: ['http://localhost:4000'],
+	origin: ['http://localhost:3000', 'https://blog-app-client-taupe-nine.vercel.app'],
     credentials: true,
     optionsSuccessStatus: 200 
 }
